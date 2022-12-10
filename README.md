bdffb755645d053bb9eff76b7bf60198647b0f11
geospiza <geospiza@protonmail.com>
2022-12-10 00:51:16 -0800

    Add scrapy scraper

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ There are other projects out there; this one is primarily for learning and fun.
 
 - https://github.com/similar-manga/similar
 - https://github.com/CarlosEsco/Neko
+
+## notes
+
+### scraping
+
+A list of all manga uuids (as of 2022-12-10) was graciously provided by tristan9 (thanks!).
+If we do need to scrape this from scratch, we filter using the `createdAtSince` parameter and paginate according to limits set by the api.
+
+```bash
+# -a limit=10 for testing
+scrapy runspider manga_recsys/scrapy/mangadex.py -a path=data/raw/2022-12-10-mangadex-uuid.csv -o data/raw/2022-12-10-mangadex-manga.ndjson -t jsonlines
+```
---
+++ manga_recsys/scrapy/mangadex.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import scrapy
+
+BASE_URI = "https://api.mangadex.org/manga"
+
+
+class MangaSpider(scrapy.Spider):
+    name = "mangaspider"
+
+    # 5 requests per second rate limit
+    custom_settings = {
+        "AUTOTHROTTLE_ENABLED": True,
+        "AUTOTHROTTLE_START_DELAY": 1,
+        "AUTOTHROTTLE_TARGET_CONCURRENCY": 4,
+        "DOWNLOAD_DELAY": 1,
+        "LOG_LEVEL": "WARNING",
+    }
+
+    def __init__(self, path, limit=None, **kwargs):
+        df = pd.read_csv(path)
+        if limit:
+            df = df.head(int(limit))
+        self.start_urls = df._id.apply(lambda x: f"{BASE_URI}/{x}").tolist()
+        super().__init__(**kwargs)
+
+    def parse(self, response):
+        yield response.json()["data"]
