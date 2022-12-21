494464efff12f7eb706d521816cb10fab756444a
geospiza <geospiza@protonmail.com>
2022-12-21 15:29:03 -0800

    Update page to always reload redoc when component changes

--- app/src/routes/openapi/+page.svelte
+++ app/src/routes/openapi/+page.svelte
@@ -1,16 +1,18 @@
 <script>
   let container;
-
   function onLoad() {
-    Redoc.init("/openapi.yaml", { scrollYOffset: 50 }, container);
+    Redoc.init("/openapi/openapi.yaml", { scrollYOffset: 50 }, container);
   }
 </script>
 
 <svelte:head>
-  <script
-    src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"
-    on:load={onLoad}
-  ></script>
+  <!-- refresh the script if we've loaded the page again -->
+  {#if container}
+    <script
+      src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"
+      on:load={onLoad}
+    ></script>
+  {/if}
 </svelte:head>
 
 <div bind:this={container} />
